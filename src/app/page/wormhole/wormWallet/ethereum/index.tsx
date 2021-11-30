4e03510d7e259cbc084e062c04f4ae31e6b6e4c9
thiennv <thiennguyen@descartes.network>
2021-11-30 15:49:13 +0700

    update code

--- src/app/page/wormhole/wormWallet/ethereum/index.tsx
+++ src/app/page/wormhole/wormWallet/ethereum/index.tsx
@@ -1,37 +1,17 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
-
-import { Avatar, Space, Typography } from 'antd'
-
-import { fetchCGK } from 'shared/helper'
+import { Space, Typography } from 'antd'
+import MintAvatar from 'app/shared/components/mintAvatar'
 
 const Ethereum = () => {
-  const [ethereumData, setEthereumData] = useState<CgkData>()
-  const { address, name, icon } = ethereumData || {}
-
-  const fetchSolanaData = useCallback(async () => {
-    const ethereumData = await fetchCGK('ethereum')
-    return setEthereumData(ethereumData)
-  }, [])
-
-  useEffect(() => {
-    fetchSolanaData()
-  }, [fetchSolanaData])
-
-  const shortenMintAddress = useMemo(() => address?.substring(0, 6), [address])
   const isConnect = false
 
   return (
     <Space>
-      <Avatar
-        src={icon}
-        size={32}
-        style={{ backgroundColor: '#2D3355', border: 'none' }}
-      />
+      <MintAvatar mintAddress={''} size={32} />
       <Space direction="vertical" size={0}>
-        <Typography.Text style={{ fontWeight: 600 }}>{name}</Typography.Text>
+        <Typography.Text style={{ fontWeight: 600 }}>Ethereum</Typography.Text>
         {isConnect && (
           <Typography.Text style={{ fontSize: 12 }}>
-            {shortenMintAddress}
+            wallet address
           </Typography.Text>
         )}
       </Space>
--- src/app/page/wormhole/wormWallet/solana/index.tsx
+++ src/app/page/wormhole/wormWallet/solana/index.tsx
@@ -1,41 +1,17 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
-
-import { Avatar, Space, Typography } from 'antd'
-
-import { fetchCGK } from 'shared/helper'
-import { useWallet } from 'senhub/providers'
+import { Space, Typography } from 'antd'
+import MintAvatar from 'app/shared/components/mintAvatar'
 
 const Solana = () => {
-  const [solanaData, setSolanaData] = useState<CgkData>()
-  const { name, icon } = solanaData || {}
-  const {
-    wallet: { address },
-  } = useWallet()
-
-  const fetchSolanaData = useCallback(async () => {
-    const solanaData = await fetchCGK('solana')
-    return setSolanaData(solanaData)
-  }, [])
-
-  useEffect(() => {
-    fetchSolanaData()
-  }, [fetchSolanaData])
-
-  const shortenMintAddress = useMemo(() => address?.substring(0, 6), [address])
   const isConnect = false
 
   return (
     <Space>
-      <Avatar
-        src={icon}
-        size={32}
-        style={{ backgroundColor: '#2D3355', border: 'none' }}
-      />
+      <MintAvatar mintAddress={''} size={32} />
       <Space direction="vertical" size={0}>
-        <Typography.Text style={{ fontWeight: 600 }}>{name}</Typography.Text>
+        <Typography.Text style={{ fontWeight: 600 }}>Solana</Typography.Text>
         {isConnect && (
           <Typography.Text style={{ fontSize: 12 }}>
-            {shortenMintAddress}
+            wallet address
           </Typography.Text>
         )}
       </Space>
