b80a97b5aeaa2443d6eef86d62c98953a2ecf59e
dmcong <83624415+dmcong@users.noreply.github.com>
2021-12-02 13:57:33 +0700

    .

--- src/app/page/history/bridge.tsx
+++ src/app/page/history/bridge.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import { Button, Col, Row, Space, Table, Typography } from 'antd'
-import { shortenAddress } from 'shared/util'
-import MintAvatar from 'app/shared/components/mintAvatar'
 import StatusTag from './statusTags'
 import IonIcon from 'shared/ionicon'
+import { MintAvatar } from 'app/shared/components/mint'
+
+import { shortenAddress } from 'shared/util'
 
 const COLUMNS = [
   {
@@ -54,190 +55,45 @@ const COLUMNS = [
   },
 ]
 
-const DATA = [
-  {
-    key: 1,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's239873434973243s239873434973243s239873434973243',
-    sourceTarget: {
-      tokenA: '5YwUkPdXLoujGkZuo9B4LsLKj3hdkDcfP4derpspifSJ',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'success',
-    action: 'open-outline',
+const DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((key) => ({
+  key: key,
+  time: '16 Nov, 2021 16:00',
+  transactionID: 's239873434973243s239873434973243s239873434973243',
+  sourceTarget: {
+    tokenA: '5YwUkPdXLoujGkZuo9B4LsLKj3hdkDcfP4derpspifSJ',
+    tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
+    titleA: 'SOL',
+    titleB: 'SEN',
   },
-  {
-    key: 2,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's2398734...34973243',
-    sourceTarget: {
-      tokenA: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'pending',
-    action: 'open-outline',
-  },
-  {
-    key: 3,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's2398734...34973243',
-    sourceTarget: {
-      tokenA: '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'pending',
-    action: 'open-outline',
-  },
-  {
-    key: 4,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's2398734...34973243',
-    sourceTarget: {
-      tokenA: '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'error',
-    action: 'open-outline',
-  },
-  {
-    key: 5,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's2398734...34973243',
-    sourceTarget: {
-      tokenA: '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'error',
-    action: 'open-outline',
-  },
-  {
-    key: 6,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's239873434973243s239873434973243s239873434973243',
-    sourceTarget: {
-      tokenA: '5YwUkPdXLoujGkZuo9B4LsLKj3hdkDcfP4derpspifSJ',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'success',
-    action: 'open-outline',
-  },
-  {
-    key: 7,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's2398734...34973243',
-    sourceTarget: {
-      tokenA: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'pending',
-    action: 'open-outline',
-  },
-  {
-    key: 8,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's2398734...34973243',
-    sourceTarget: {
-      tokenA: '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'pending',
-    action: 'open-outline',
-  },
-  {
-    key: 9,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's2398734...34973243',
-    sourceTarget: {
-      tokenA: '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'error',
-    action: 'open-outline',
-  },
-  {
-    key: 10,
-    time: '16 Nov, 2021 16:00',
-    transactionID: 's2398734...34973243',
-    sourceTarget: {
-      tokenA: '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c',
-      tokenB: '2z6Ci38Cx6PyL3tFrT95vbEeB3izqpoLdxxBkJk2euyj',
-      titleA: 'SOL',
-      titleB: 'SEN',
-    },
-    amount: '18.5 SOL',
-    status: 'error',
-    action: 'open-outline',
-  },
-]
-
-const DATA_LENGHT = DATA.length
-
-const DISPLAY_NONE = {
-  display: 'none',
-}
+  amount: '18.5 SOL',
+  status: 'success',
+  action: 'open-outline',
+}))
 
 const Bridge = () => {
   const [amountRow, setAmountRow] = useState(4)
-  const [isDisplayNone, setIsDisplayNone] = useState(false)
 
-  useEffect(() => {
-    setIsDisplayNone(amountRow >= DATA_LENGHT)
-  }, [amountRow])
-
-  const onHanldeViewMore = () => {
-    if (amountRow < DATA_LENGHT) {
-      setAmountRow(amountRow + 4)
-    }
-  }
-
+  const onHandleViewMore = () => setAmountRow(amountRow + 4)
   return (
     <Row gutter={[16, 16]} justify="center">
       <Col span={24}>
         <Table
           columns={COLUMNS}
           dataSource={DATA.slice(0, amountRow)}
-          rowClassName={(record, index) =>
-            index % 2 === 0 ? 'even-row' : 'odd-row'
-          }
+          rowClassName={(record, index) => (index % 2 ? 'odd-row' : 'even-row')}
           pagination={false}
           scroll={{ x: 1000, y: 240 }}
         />
       </Col>
       <Col>
-        <Button
-          onClick={() => onHanldeViewMore()}
-          icon={<IonIcon name="chevron-down-outline" />}
-          style={isDisplayNone ? DISPLAY_NONE : {}}
-        >
-          View more
-        </Button>
+        {amountRow < DATA.length && (
+          <Button
+            onClick={onHandleViewMore}
+            icon={<IonIcon name="chevron-down-outline" />}
+          >
+            View more
+          </Button>
+        )}
       </Col>
     </Row>
   )
