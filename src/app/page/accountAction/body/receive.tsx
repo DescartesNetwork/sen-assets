5234a0592afe0ea0c0a83fa2ff7db3cb9cdd1a44
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2021-12-06 13:51:29 +0700

    Remove check in Receive.tsx

--- src/app/page/accountAction/body/receive.tsx
+++ src/app/page/accountAction/body/receive.tsx
@@ -1,34 +1,27 @@
-import { account } from '@senswap/sen-js'
 import { Col, Input, Row, Typography } from 'antd'
 import QRcode from 'qrcode.react'
 
 const Receive = ({ accountAddr }: { accountAddr: string }) => {
   return (
     <Row justify="center" gutter={[16, 16]}>
-      {account.isAddress(accountAddr) ? (
-        <>
-          <Col style={{ background: '#f4f4f5', paddingTop: 8 }}>
-            <QRcode
-              value={accountAddr}
-              size={84}
-              bgColor="#ffffff"
-              fgColor="#1f1f1f"
-            />
+      <Col style={{ background: '#f4f4f5', paddingTop: 8 }}>
+        <QRcode
+          value={accountAddr}
+          size={84}
+          bgColor="#ffffff"
+          fgColor="#1f1f1f"
+        />
+      </Col>
+      <Col span={24}>
+        <Row gutter={[8, 8]}>
+          <Col span={24}>
+            <Typography.Text>SOL receive address</Typography.Text>
           </Col>
           <Col span={24}>
-            <Row gutter={[8, 8]}>
-              <Col span={24}>
-                <Typography.Text>SOL receive address</Typography.Text>
-              </Col>
-              <Col span={24}>
-                <Input size="large" value={accountAddr} />
-              </Col>
-            </Row>
+            <Input size="large" value={accountAddr} />
           </Col>
-        </>
-      ) : (
-        <Typography.Text>No data</Typography.Text>
-      )}
+        </Row>
+      </Col>
     </Row>
   )
 }
