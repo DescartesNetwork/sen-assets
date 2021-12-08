34f8e38ef9995f0a70b21c6eecae907a335d419d
dmcong <83624415+dmcong@users.noreply.github.com>
2021-12-08 09:56:12 +0700

    Update receive.tsx

--- src/app/page/accountAction/body/receive.tsx
+++ src/app/page/accountAction/body/receive.tsx
@@ -1,25 +1,59 @@
-import { Col, Row, Typography } from 'antd'
+import { useCallback, useEffect, useState } from 'react'
+
+import { Col, Row, Space, Switch, Typography } from 'antd'
 import InputCopy from 'app/components/inputCopy'
 import QRcode from 'qrcode.react'
 
+import { useAccount, useWallet } from 'senhub/providers'
+
 const Receive = ({ accountAddr }: { accountAddr: string }) => {
+  const { accounts } = useAccount()
+  const { wallet } = useWallet()
+  const [devMode, setDevMode] = useState(false)
+  const [addressDisplay, setAddressDisplay] = useState('')
+
+  const getAccountWithMode = useCallback(async () => {
+    if (!devMode || accountAddr === wallet.address)
+      return setAddressDisplay(wallet.address)
+
+    const splt = window.sentre.splt
+    const mint = accounts[accountAddr].mint
+    const deriveAddress = await splt.deriveAssociatedAddress(
+      wallet.address,
+      mint,
+    )
+    return setAddressDisplay(deriveAddress)
+  }, [accountAddr, accounts, devMode, wallet.address])
+
+  useEffect(() => {
+    getAccountWithMode()
+  }, [getAccountWithMode])
+
   return (
     <Row justify="center" gutter={[16, 16]}>
       <Col style={{ background: '#f4f4f5', paddingTop: 8 }}>
         <QRcode
-          value={accountAddr}
-          size={84}
+          value={addressDisplay}
+          size={110}
           bgColor="#ffffff"
           fgColor="#1f1f1f"
         />
       </Col>
       <Col span={24}>
         <Row gutter={[8, 8]}>
-          <Col span={24}>
+          <Col flex="auto">
             <Typography.Text>Wallet address</Typography.Text>
           </Col>
+          <Col>
+            {accountAddr !== wallet.address && (
+              <Space>
+                <Typography.Text>Dev mode</Typography.Text>
+                <Switch size="small" checked={devMode} onChange={setDevMode} />
+              </Space>
+            )}
+          </Col>
           <Col span={24}>
-            <InputCopy size="large" value={accountAddr} />
+            <InputCopy size="large" value={addressDisplay} />
           </Col>
         </Row>
       </Col>
