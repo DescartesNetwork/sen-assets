9159a953f7038283c999a80bcdbfd73d1347775a
dmcong <83624415+dmcong@users.noreply.github.com>
2021-12-16 09:22:21 +0700

    rename

--- src/app/page/history/wormhole/column.tsx
+++ src/app/page/history/wormhole/column.tsx
@@ -3,7 +3,7 @@ import moment from 'moment'
 import { Space, Typography } from 'antd'
 import NetworkAvatar from 'app/components/network/networkAvatar'
 import ColumAction from './columnAction'
-import HistoryStatus from './status'
+import HistoryStatus from './columnStatus'
 import NetworkName from 'app/components/network/networkName'
 
 import { shortenAddress } from 'shared/util'
