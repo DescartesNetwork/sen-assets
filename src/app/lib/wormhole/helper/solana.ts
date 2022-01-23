fadba12de623e85f8f513588ddb45f195e11e4b2
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-01-23 08:17:19 +0700

    .

--- src/app/lib/wormhole/helper/solana.ts
+++ src/app/lib/wormhole/helper/solana.ts
@@ -1,6 +1,31 @@
 import { Connection } from '@solana/web3.js'
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
 
 export const getSolConnection = () => {
   const nodeUrl = window.sentre.splt.nodeUrl
   return new Connection(nodeUrl, 'confirmed')
+}
+
+export const getSolTokens = async (address: string) => {
+  // const MY_WALLET_ADDRESS = 'FriELggez2Dy3phZeHHAdpcoEXkKQVkv6tx3zDtCVP8T'
+  const nodeUrl = window.sentre.splt.nodeUrl
+  const connection = new Connection(nodeUrl, 'confirmed')
+
+  const tokens = await connection.getParsedProgramAccounts(
+    TOKEN_PROGRAM_ID, // new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+    {
+      filters: [
+        {
+          dataSize: 165, // number of bytes
+        },
+        {
+          memcmp: {
+            offset: 32, // number of bytes
+            bytes: address, // base58 encoded string
+          },
+        },
+      ],
+    },
+  )
+  return tokens
 }
